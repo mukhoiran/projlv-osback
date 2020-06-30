6cb20001509a6bd27b573c88b401ead505769763
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-06-30 19:40:23 +0700

    remove register and add adminlte assets

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
+
+//remove route register
+Route::match(["GET", "POST"], "/register", function(){
+return redirect("/login");
+})->name("register");
 
 Route::get('/home', 'HomeController@index')->name('home');
